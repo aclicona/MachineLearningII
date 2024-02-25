bcf9f38348a2f1c9e8934cd3b7a5f9ba133698c9
Andrés Licona <aclicona@gmail.com>
2024-02-25 12:55:23 -0500

    Update

--- Taller3/http_question_answering/question_answering/q_a/question_answering.py
+++ Taller3/http_question_answering/question_answering/q_a/question_answering.py
@@ -9,6 +9,6 @@ def get_most_related_paragraphs(model, text, question, number_of_chunks: int = 3
     scores = util.cos_sim(embeddings[:1], embeddings[1:]) * 100
     order = np.argsort(scores.tolist()[0])[::-1].astype(int)
     text = np.array(text)
-    if len(text) <= number_of_chunks:
+    if number_of_chunks <= len(text):
         return text[order[:number_of_chunks]]
     return text[order]
--- Taller3/http_question_answering/question_answering/q_a/views.py
+++ Taller3/http_question_answering/question_answering/q_a/views.py
@@ -7,12 +7,17 @@ from .question_answering import get_most_related_paragraphs
 class MyFormView(FormView):
     template_name = "form.html"
     form_class = QuestionForm
-    success_url = "success_url"  # Redirect URL on form success
 
     def form_valid(self, form):
         context = self.get_context_data(form=form)
         question = form.cleaned_data['question']
-        context['question'] = f'Your question: {question}'
-        top_3_related_paragraphs = get_most_related_paragraphs(EMBEDDING_MODEL, TEXT_BASE, question)
-        context['more'] = f'Your question: {question}'
+        context['question'] = question
+        most_related_paragraphs = get_most_related_paragraphs(EMBEDDING_MODEL, TEXT_BASE, question)
+        context['most_related'] = most_related_paragraphs
+        context['text'] = TEXT_BASE
         return self.render_to_response(context=context)
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['text'] = TEXT_BASE
+        return self.render_to_response(context)
